a18830e5417a018d7ae52887f9bfb6b292751192
unknown <you@example.com>
2020-10-01 00:32:07 +0530

    first commit

---
+++ day 1/evenodd.php
@@ -0,0 +1,13 @@
+<?php
+
+$num=array(11,23,72,44);
+for($i=0;$i<4;$i++)
+  {
+    $res=$num[$i];
+    if(($res%2)==0)
+      echo "The number ".$res." is an even number.<br>";
+    else
+      echo "The number ".$res." is an odd number.<br>";
+  }
+
+?>
---
+++ day 1/vowel.php
@@ -0,0 +1,20 @@
+<?php
+
+$var="a" ;
+
+switch($var)
+{
+  case "a" : echo "It is a vowel ".$var."." ;
+    break;
+  case "e" : echo "It is a vowel ".$var.".";
+    break;
+  case "i" : echo "It is a vowel ".$var."." ;
+    break;
+  case "o" : echo "It is a vowel ".$var."." ;
+    break;
+  case "u" : echo "It is a vowel ".$var."." ;
+    break;
+  default : echo "Its a Consonant.";
+}
+
+?>
